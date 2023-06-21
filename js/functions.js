0db086b4abe1eeea6d4955eede8f1ae7f6fd326d
Dmyrto Klymenko <delovar1@gmil.com>
2023-06-21 13:18:42 +0300

    Создаст фукции для тренировки

---
+++ js/functions.js
@@ -0,0 +1,43 @@
+function checkStringLength (string, length) {
+  if (string.length > length) {
+    return false;
+  }
+
+  return true;
+}
+
+function isPalindrome (string) {
+  const normalizedString = string.replaceAll(' ','').toLowerCase();
+
+  let reverseString = '';
+
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reverseString += normalizedString[i];
+  }
+
+  if (normalizedString === reverseString) {
+    return true;
+  }
+
+  return false;
+}
+
+function getDigitsFromString (string) {
+  const normalizedString = string.toString().replaceAll(' ','').toLowerCase();
+
+  let numbersString = '';
+
+  for (let i = 0; i < normalizedString.length; i++) {
+    const symbol = normalizedString[i];
+
+    if (!Number.isNaN(Number(symbol))) {
+      numbersString += symbol;
+    }
+  }
+
+  if(Number(numbersString) === 0) {
+    return NaN;
+  }
+
+  return Number(numbersString);
+}
