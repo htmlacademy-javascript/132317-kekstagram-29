50c56f8c210b903c383ea5f0fadfd8446f10ebbd
Dmyrto Klymenko <delovar1@gmil.com>
2023-08-01 12:33:25 +0300

    module9-task1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import './util.js';
 import {getPictures} from './data.js';
 import {fillingListPictures} from './miniatures.js';
 import './full-photos.js';
+import {openModalFormScript} from './form.js';
 
 fillingListPictures(getPictures());
+openModalFormScript();
